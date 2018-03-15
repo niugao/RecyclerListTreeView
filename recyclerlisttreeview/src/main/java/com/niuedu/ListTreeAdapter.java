22969b41db5968e93a9a9444b24d7fac0a31921d
niu <womenorman@hotmail.com>
2018-03-15 20:59:21 +0800

    update compile sdk

--- recyclerlisttreeview/src/main/java/com/niuedu/ListTreeAdapter.java
+++ recyclerlisttreeview/src/main/java/com/niuedu/ListTreeAdapter.java
@@ -2,7 +2,6 @@ package com.niuedu;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
