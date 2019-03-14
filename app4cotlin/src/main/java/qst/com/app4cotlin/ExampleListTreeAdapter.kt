13a47d86f63af650b22c4352b7e2e9f737bd20dc
niugao <womenorman@hotmail.com>
2019-03-14 16:47:17 +0800

    完成Kotlin版App代码

---
+++ app4cotlin/src/main/java/qst/com/app4cotlin/ExampleListTreeAdapter.kt
@@ -0,0 +1,134 @@
+package qst.com.app4cotlin
+
+import android.graphics.Bitmap
+import android.support.v7.widget.PopupMenu
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Switch
+import android.widget.TextView
+import com.niuedu.ListTree
+import com.niuedu.ListTreeAdapter
+
+class ExampleListTreeAdapter(tree: ListTree, listener : PopupMenu.OnMenuItemClickListener) :
+        ListTreeAdapter<ExampleListTreeAdapter.BaseViewHolder>(tree){
+
+    //行上弹出菜单的侦听器
+    private val itemMenuClickListener : PopupMenu.OnMenuItemClickListener
+
+    //记录弹出菜单是在哪个行上出现的
+    var currentNode: ListTree.TreeNode? = null
+
+    //保存子行信息的类
+    class ContactInfo(
+            val bitmap: Bitmap, //头像,用于设置给ImageView
+            val title: String, //标题
+            val detail: String //描述
+    )
+
+    init{
+        itemMenuClickListener = listener
+    }
+
+    override fun onCreateNodeView(parent: ViewGroup?, viewType: Int): BaseViewHolder? {
+        val inflater = LayoutInflater.from(parent!!.getContext())
+
+        //创建不同的行View
+        if (viewType == R.layout.contacts_group_item) {
+            //注意！此处有一个不同！最后一个参数必须传true！
+            val view = inflater.inflate(viewType, parent, true)
+            //用不同的ViewHolder包装
+            return GroupViewHolder(view)
+        } else if (viewType == R.layout.contacts_contact_item) {
+            //注意！此处有一个不同！最后一个参数必须传true！
+            val view = inflater.inflate(viewType, parent, true)
+            //用不同的ViewHolder包装
+            return ContactViewHolder(view)
+        } else {
+            return null
+        }
+    }
+
+    override fun onBindNodeViewHolder(viewHoler: BaseViewHolder?, position: Int) {
+        //get node at the position
+        val node = tree.getNodeByPlaneIndex(position)
+
+        if (node.layoutResId == R.layout.contacts_group_item) {
+            //group node
+            val title = node.data as String
+
+            val gvh = viewHoler as GroupViewHolder
+            gvh.textViewTitle.text = title
+            gvh.textViewCount.text = "0/" + node.childrenCount
+            gvh.aSwitch.isChecked = node.isChecked
+        } else if (node.layoutResId == R.layout.contacts_contact_item) {
+            //child node
+            val info = node.data as ContactInfo
+
+            val cvh = viewHoler as ContactViewHolder
+            cvh.imageViewHead.setImageBitmap(info.bitmap)
+            cvh.textViewTitle.text = info.title
+            cvh.textViewDetail.text = info.detail
+            cvh.aSwitch.isChecked = node.isChecked
+        }
+    }
+
+    //组行和联系人行的Holder基类
+    open inner class BaseViewHolder(itemView: View) : ListTreeViewHolder(itemView)
+
+    //将ViewHolder声明为Adapter的内部类，反正外面也用不到
+    internal inner class GroupViewHolder(itemView: View) : BaseViewHolder(itemView) {
+
+        var textViewTitle: TextView = itemView.findViewById(R.id.textViewTitle)
+        var textViewCount: TextView = itemView.findViewById(R.id.textViewCount)
+        var aSwitch: Switch = itemView.findViewById(R.id.switchChecked)
+        var textViewMenu: TextView = itemView.findViewById(R.id.textViewMenu)
+
+        init {
+
+            //应响应点击事件而不是CheckedChange事件，因为那样会引起事件的递归触发
+            aSwitch.setOnClickListener {
+                val planeIndex = adapterPosition
+                val node = tree.getNodeByPlaneIndex(planeIndex)
+                node.isChecked = !node.isChecked
+                //改变所有的子孙们的状态
+                val count = tree.setDescendantChecked(planeIndex, node.isChecked)
+                notifyItemRangeChanged(planeIndex, count + 1)
+            }
+
+            //点了PopMenu控件，弹出PopMenu
+            textViewMenu.setOnClickListener { v ->
+                val nodePlaneIndex = adapterPosition
+                val node = tree.getNodeByPlaneIndex(nodePlaneIndex)
+                currentNode = node
+                val popup = PopupMenu(v.context, v)
+                popup.setOnMenuItemClickListener(itemMenuClickListener)
+                val inflater = popup.menuInflater
+                inflater.inflate(R.menu.menu_item, popup.menu)
+                popup.show()
+            }
+        }
+    }
+
+    internal inner class ContactViewHolder(itemView: View) : BaseViewHolder(itemView) {
+        var imageViewHead: ImageView = itemView.findViewById(R.id.imageViewHead)
+        var textViewTitle: TextView = itemView.findViewById(R.id.textViewTitle)
+        var textViewDetail: TextView = itemView.findViewById(R.id.textViewDetail)
+        var aSwitch: Switch = itemView.findViewById(R.id.switchChecked)
+
+        init {
+
+            //应响应点击事件而不是CheckedChange事件，因为那样会引起事件的递归触发
+            aSwitch.setOnClickListener {
+                val nodePlaneIndex = adapterPosition
+                val node = tree.getNodeByPlaneIndex(nodePlaneIndex)
+                node.isChecked = !node.isChecked
+                //改变所有的子孙们的状态
+                val count = tree.setDescendantChecked(nodePlaneIndex, node.isChecked)
+                notifyItemRangeChanged(nodePlaneIndex, count + 1)
+            }
+        }
+    }
+
+}
--- app4cotlin/src/main/java/qst/com/app4cotlin/MainActivity.kt
+++ app4cotlin/src/main/java/qst/com/app4cotlin/MainActivity.kt
@@ -1,14 +1,24 @@
 package qst.com.app4cotlin
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PopupMenu
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
-
+import com.niuedu.ListTree
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , PopupMenu.OnMenuItemClickListener{
+
+    //保存数据的集合
+    private val tree = ListTree()
+
+    //从ListTreeAdapter派生的Adapter
+    internal var adapter: ExampleListTreeAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +29,42 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        //创建后台数据：一棵树
+        //创建组们，是root node，所有parent为null
+        val groupNode1 = tree.addNode(null, "特别关心", R.layout.contacts_group_item)
+        val groupNode2 = tree.addNode(null, "我的好友", R.layout.contacts_group_item)
+        val groupNode3 = tree.addNode(null, "朋友", R.layout.contacts_group_item)
+        val groupNode4 = tree.addNode(null, "家人", R.layout.contacts_group_item)
+        val groupNode5 = tree.addNode(null, "同学", R.layout.contacts_group_item)
+
+        //第二层
+        var contact: ExampleListTreeAdapter.ContactInfo
+        var bitmap = BitmapFactory.decodeResource(resources, R.drawable.contacts_normal)
+        contact = ExampleListTreeAdapter.ContactInfo(bitmap, "王二", "[在线]我是王二")
+        val contactNode1 = tree.addNode(groupNode2, contact, R.layout.contacts_contact_item)
+        val contactNode2 = tree.addNode(groupNode5, contact, R.layout.contacts_contact_item)
+        //再添加一个
+        bitmap = BitmapFactory.decodeResource(resources, R.drawable.contacts_normal)
+        contact = ExampleListTreeAdapter.ContactInfo(bitmap, "王三", "[离线]我没有状态")
+        tree.addNode(groupNode2, contact, R.layout.contacts_contact_item)
+        tree.addNode(groupNode5, contact, R.layout.contacts_contact_item)
+
+        //第三层
+        bitmap = BitmapFactory.decodeResource(resources, R.drawable.contacts_normal)
+        contact = ExampleListTreeAdapter.ContactInfo(bitmap, "东邪", "[离线]出来还价")
+        var n: ListTree.TreeNode = tree.addNode(contactNode1, contact, R.layout.contacts_contact_item)
+        n.isShowExpandIcon = false
+        //再添加一个
+        bitmap = BitmapFactory.decodeResource(resources, R.drawable.contacts_normal)
+        contact = ExampleListTreeAdapter.ContactInfo(bitmap, "李圆圆", "[离线]昨天出门没出去")
+        n = tree.addNode(contactNode1, contact, R.layout.contacts_contact_item)
+        n.isShowExpandIcon = false
+
+        adapter = ExampleListTreeAdapter(tree, this)
+        listView.layoutManager = LinearLayoutManager(this)
+        listView.setAdapter(adapter)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -28,12 +74,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val id = item.itemId
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_del_selected) {
+            //删除选中的Nodes，删一个Node时会将其子孙一起删掉
+            tree.removeCheckedNodes()
+            adapter?.notifyDataSetChanged()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_add_item -> {
+                //向当前行增加一个儿子
+                val node = adapter!!.currentNode
+                val bitmap = BitmapFactory.decodeResource(resources, R.drawable.contacts_normal)
+                val contact = ExampleListTreeAdapter.ContactInfo(
+                        bitmap, "New contact", "[离线]我没有状态")
+                val childNode = tree.addNode(node, contact, R.layout.contacts_contact_item)
+                adapter!!.notifyTreeItemInserted(node, childNode)
+                return true
+            }
+            R.id.action_clear_children -> {
+                //清空所有的儿子们
+                val node = adapter!!.currentNode
+                val range = tree.clearDescendant(node)
+                adapter!!.notifyItemRangeRemoved(range!!.first, range.second)
+                return true
+            }
+            else -> return false
         }
     }
 }
